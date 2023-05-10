ff903e4bb28dc6d34832216c64078c7e2e3abe52
Derekzcx <847256511@qq.com>
2023-05-10 21:13:03 +0800

    修改

--- C++_排序（合并版）/Sorts3.cpp
+++ C++_排序（合并版）/Sorts3.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 /******************** 算法函数 ************************/
+// 冒泡 n^2 
 void Bubble_Sort(vector<int> &vec){
 	for(int i=0; i<vec.size()-1; i++){
 		for(int j = i+1; j<vec.size(); j++){
@@ -15,6 +16,50 @@ void Bubble_Sort(vector<int> &vec){
 		}
 	}
 } 
+// 快排 nlogn 
+void Quick_Sort(vector<int> &vec, int l, int r){
+	if(l>=r) return;
+	int base_val = vec[l];
+	int left = 1;
+	int right = r;
+	bool flag = false;
+	while(l!=r){
+		if(!flag){
+			if(vec[r] < base_val){
+				vec[l] = vec[r];
+				l++;
+				flag = true;
+			}else{
+				r--;
+			}
+		}else{
+			if(vec[l] > base_val){
+				vec[r] = vec[l];
+				r--;
+				flag = false;
+			}else{
+				l++;
+			}
+		}
+	}
+	vec[l] = base_val;
+	Quick_Sort(vec, left, l);
+	Quick_Sort(vec, l+1, right);
+}
+
+// 插入排序 n^2
+void Insert_Sort(vector<int> &vec){
+	for(int i=1; i<vec.size(); i++){
+		for(int j=i-1; j>=0; j--){
+			if(vec[j+1] < vec[j]) swap(vec[j], vec[j+1]);
+			else break;
+		}
+	}
+} 
+
+void Shell_Sort(vector<int> &vec){
+	
+}
 
 /******************** 工具函数 ************************/
 void create_testval(vector<int> &vec){
@@ -41,7 +86,12 @@ int main(){
 	for_each(vec.begin(), vec.end(),printval);
 	printf("\n");
 	printf("alfter: ");
-	Bubble_Sort(vec);
+	// 冒泡 
+	//Bubble_Sort(vec);
+	// 快排 
+	//Quick_Sort(vec, 0, vec.size()-1); 
+	// 插入排序
+	Insert_Sort(vec); 
 	for_each(vec.begin(), vec.end(),printval);
 	return 0;
 }
